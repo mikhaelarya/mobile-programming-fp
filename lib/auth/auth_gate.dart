1b6ed72b314edc105427e71e2f447a426b9ff8ba
mikhaelarya <mikabel.arya@gmail.com>
2024-06-25 17:08:48 +0700

    delete const

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -14,7 +14,7 @@ class AuthGate extends StatelessWidget {
         builder: (context, snapshot) {
           // user is logged in
           if (snapshot.hasData) {
-            return const HomePage();
+            return HomePage();
           }
           // user is not logged in
           else {
